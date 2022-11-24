1f4f17bda80e19c436ee1e193a52f3dd6acb9cc2
Aleksey Efimov <smaylik121@gmail.com>
2022-11-24 13:57:19 +0300

    Initial Commit

---
+++ Lesson 2.1 Conscraint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.1 Conscraint
+//
+//  Created by Aleksey on 24.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
